7eaf57e34b2e00231c2061f273d84061bb31ce70
WojciechSala <wojteksala190@gmail.com>
2018-08-12 16:56:13 +0200

    header's code reduction

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="header-container">
-    <div class="logo">CODE-REVIEW</div>
+	<div class="header-container">
+	<div class="logo">CODE-REVIEW</div>
 
-    <i class="el-icon-more">sd
-      <ul class="dropdown-content">
-        <li>Log In</li>
-        <li>Sign Up</li>
-        <li>Team</li>
-        <li>Coworking</li>
-        <li>Github</li>
-      </ul>
-    </i>
+	<i class="el-icon-more icon-list">
+		<ul class="dropdown-content">
+			<li>Log In</li>
+			<li>Sign Up</li>
+			<li>Team</li>
+			<li>Coworking</li>
+			<li>Github</li>
+	  	</ul>
+	</i>
 
-    <div class="nav-container__left">     
-      <a href="">Team</a>
-      <a href="">Coworking</a>
-      <a href="">Github</a>
-    </div>
+	<div class="nav-container__left">     
+		<a href="">Team</a>
+	  	<a href="">Coworking</a>
+	  	<a href="">Github</a>
+	</div>
 
-    <div class="nav-container__right">
-      <a href="">Log In</a>
-      <a href="">Sign Up</a>
-    </div>
-  </div>
+	<div class="nav-container__right">
+		<a href="">Log In</a>
+	  	<a href="">Sign Up</a>
+	</div>
+  	</div>
 </template>
 
 <script>
@@ -30,7 +30,7 @@ export default {}
 import Vue from 'vue'
 
 import {
-  Icon
+	Icon
 
 } from 'element-ui'
 
@@ -45,231 +45,133 @@ $leading: rgb(0, 139, 219);
 $lightGrey: #d4d4d4;
 $darkGrey: rgb(17, 17, 17);
 
-//main
-.logo{
-  font-family: 'Monoton', cursive;
-  color: $leading;
-  float: left;
-  text-align: center;
-}
-a{
-  text-decoration: none;
-  color: $lightGrey;
-  font-size: 16px;
-
-    &:hover{
-      animation-name: TextFadeIn;
-      animation-duration: 1.5s;
-      color: $leading;
-    }
-}
-.nav-container__left{
-  display: inline-block;
-
-  a{
-    float: left;
-    margin: 1.5em 0 0 2em;
-    
-    &:first-of-type{
-      margin-left: 2em;
-    }
-  }
-}
-.nav-container__right{
-  float: right;
-  margin-top: 1.5em;
-  
-  a{
-    margin-right: 1.5em;
-  }
-}
-
 //animations
 @keyframes TextFadeIn {
-  from{color: $lightGrey;}
-  to  {color: $leading;}
+	from{color: $lightGrey;}
+	to  {color: $leading;}
 }
 
+//main
+.logo{
+	font-family: 'Monoton', cursive;
+	color: $leading;
+	float: left;
+	text-align: center;
+}
+a{
+	text-decoration: none;
+	color: $lightGrey;
+	font-size: 16px;
+	visibility: hidden;
+
+	&:hover{
+		animation-name: TextFadeIn;
+		animation-duration: 1.5s;
+		color: $leading;
+	}
+}
+.nav-container__left{
+	display: inline-block;
+
+	a{
+		float: left;
+		margin: 1.5em 0 0 2em;
+		
+		&:first-of-type{ margin-left: 2em; }
+	}
+}
+.nav-container__right{
+	float: right;
+  	margin-top: 1.5em;
+  
+  	a{ margin-right: 1.5em; }
+}
+.dropdown-content{
+	width: 100%;
+	font-size: 20px;
+	display: none;
+	list-style: none;
+	position: absolute;
+	right: 0em;
+	background-color: $darkGrey;
+	margin-top: -.1em;
+
+	li{ padding: 10px 20px 10px 20px; }
+}
+.icon-list{
+	float: right;
+	color: $lightGrey;
+	cursor: pointer;
+	text-align: center;
+	width: 100px;
+
+	&:hover{
+		animation-name: TextFadeIn;
+		animation-duration: 1.5s;
+		color: $leading;
+
+		.dropdown-content{
+			display: block;
+			
+			li{
+				color: $lightGrey;
+				
+				&:hover{
+					animation-name: TextFadeIn;
+					animation-duration: 1.5s;
+					color: $leading;
+				}
+			}
+		}
+	}
+
+}
 //smartphones
-@media only screen and (max-width: 320px){
-  .logo{
-    margin: 0.3em 0 0 0.5em;
-    text-align: left;
-  }
-    //dropdown
-  .icon-list{
-    font-size: 40px;
-    float: right;
-    margin: -1.1em 0.3em 0 0;
-    color: $lightGrey;
-    cursor: pointer;
-    text-align: center;
-
-    &:hover{
-      animation-name: TextFadeIn;
-      animation-duration: 1.5s;
-      color: $leading;
-
-      .dropdown-content{
-        display: block;
-
-        li{
-          color: $lightGrey;
-
-            &:hover{
-              animation-name: TextFadeIn;
-              animation-duration: 1.5s;
-              color: $leading;
-            }
-        }
-      }
-    }
-    .dropdown-content{
-      padding: 0;
-      display: none;
-      position: absolute;
-      right: 0;
-      margin-top: 0em;
-      list-style: none;
-      font-size: 20px;
-      background-color: $darkGrey;
-      width: 100%;
-
-      li{
-        //border-top: $leading solid 1px;
-        padding: 10px 20px 10px 20px;
-      }
-    }
-  }
-  a{
-    visibility: hidden;
-  }
+@media only screen and (max-width: 321px){
+  	.logo{
+		margin: 0.3em 0 0 0.5em;
+		text-align: left;
+  	}
+	.icon-list{
+		font-size: 20px;
+		margin: -1.2em -2em 0 0;
+  	}
 }
 
 @media only screen and (max-width: 599px){
-  .logo{
-    font-size: 25px;
-    margin-top: 0.3em;
-    width: 100%;
-  }
-  //dropdown
-  .icon-list{
-    font-size: 40px;
-    float: right;
-    margin: -1.1em 0.3em 0 0;
-    color: $lightGrey;
-    cursor: pointer;
-    text-align: center;
-
-    &:hover{
-      animation-name: TextFadeIn;
-      animation-duration: 1.5s;
-      color: $leading;
-
-      .dropdown-content{
-        display: block;
-        
-        li{
-          color: $lightGrey;
-          
-          &:hover{
-            animation-name: TextFadeIn;
-            animation-duration: 1.5s;
-            color: $leading;
-          }
-        }
-      }
-    }
-    .dropdown-content{
-      padding: 0;
-      display: none;
-      position: absolute;
-      right: 0;
-      margin-top: 0em;
-      list-style: none;
-      font-size: 20px;
-      background-color: $darkGrey;
-      width: 100%;
-
-      li{
-        //border-top: $leading solid 1px;
-        padding: 10px 20px 10px 20px;
-      }
-    }
-  }
-  a{
-    visibility: hidden;
-  }
+  	.logo{
+		font-size: 25px;
+		margin-top: 0.3em;
+		width: 100%;
+  	}
+	.icon-list{
+		font-size: 35px;
+		margin: -1em .8em 0 0;
+		width: auto;
+  	}
 }
 
 //tablets
 @media only screen and (min-width: 600px) and (max-width: 767px){
-  .logo{
-    font-size: 30px;
-    margin: 0.3em auto;
-    width: 100%;
-  }
-  //dropdown
-  .icon-list{
-    font-size: 50px;
-    float: right;
-    margin: -1.2em 0.5em 0 0;
-    color: $lightGrey;
-    cursor: pointer;
-    text-align: center;
-    width: 120px;
-
-    &:hover{
-      animation-name: TextFadeIn;
-      animation-duration: 1.5s;
-      color: $leading;
-
-      .dropdown-content{
-        display: block;
-        
-        li{
-          color: $lightGrey;
-          
-          &:hover{
-            animation-name: TextFadeIn;
-            animation-duration: 1.5s;
-            color: $leading;
-          }
-        }
-      }
-    }
-
-    .dropdown-content{
-      padding: 0;
-      display: none;
-      position: absolute;
-      right: 0em;
-      margin-top: -.7em;
-      list-style: none;
-      font-size: 20px;
-      background-color: $darkGrey;
-      width: 100%;
-
-      li{
-        //border-top: $leading solid 1px;
-        padding: 10px 20px 10px 20px;
-      }
-    }
-  }
-  a{
-    visibility: hidden;
-  }
-
+	.logo{
+		font-size: 30px;
+		margin: 0.3em auto;
+		width: 100%;
+	}
+	.icon-list{
+		font-size: 40px;
+		margin: -1.2em 0.3em 0 0;
+  	}
 }
 
 //pc
 @media only screen and (min-width: 768px) {
-  .logo{
-    font-size: 35px;
-    margin: .2em 0 0 .5em;
-  }
-  .icon-list{
-    display: none;
-  }
+	.logo{
+		font-size: 35px;
+		margin: .2em 0 0 .5em;
+	}
+	
+	a{ visibility: visible; }
+	.icon-list{ display: none; }
 }
 </style>
