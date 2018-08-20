6173d96192291567db949c619b561084ee81c4db
WojciechSala <wojteksala190@gmail.com>
2018-08-20 12:04:06 +0200

    I didn't like the design so changing everything

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,23 @@
 <template>
-	<div id="app">
-		<router-view/>
- 	</div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'app',
+  name: 'App'
 }
 </script>
 
-<style slang="scss">
-
-@import "./stylesheets/flexbox.scss";
-
-	body{
-		background-color: #1e1e1e;
-		padding: 0;
-		margin: 0 !important;
-		font-family: 'Roboto', sans-serif;
-		font-weight: 300;
-	}
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,28 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Add from '@/components/Add'
-import Hello from '@/components/Hello'
-import Login from '@/components/LoginForm'
-
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-	routes: [
-		{
-			path: '/',
-			name: 'Hello',
-			component: Hello
-		},
-		{
-			path: '/login',
-			name: 'Login',
-			component: Login
-		},
-		{
-			path: '/review/add',
-			name: 'Add',
-			component: Add
-		}
-	]
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
