d575ddaf7c6b4cc1aec11d97139d1d04109a8d5b
WojciechSala <wojteksala190@gmail.com>
2018-08-21 18:25:13 +0200

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-    <div id="app">
-        <router-view/>
-    </div>
+	<div id="app">
+		<router-view/>
+	</div>
 </template>
 
 <script>
 export default {
-    name: 'App'
+	name: 'App'
 }
 </script>
 
@@ -19,9 +19,9 @@ $blue: rgb(68,138,255);
 
 body{
 	font-family: 'Roboto', sans-serif;
-    background-color: $white;
-    padding: 0 !important;
-    margin: 0 !important;
+	background-color: $white;
+	padding: 0 !important;
+	margin: 0 !important;
 }
 
 </style>
--- client/src/components/Welcome.vue
+++ client/src/components/Welcome.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <header class="mdl-layout__header">
-            <div class="mdl-layout__header-row">
-                <!-- Title -->
-                <span class="mdl-layout-title">Code-Review</span>
-                <!-- Add spacer, to align navigation to the right -->
-                <div class="mdl-layout-spacer"></div>
-                <nav class="mdl-navigation">
-                    <a class="mdl-navigation__link" href="">Log In</a>
-                    <a class="mdl-navigation__link" href="">Sign In</a>
-                </nav>
-            </div>
-        </header>
-        <div class="info">
-            <div class="info__text">
-                <h1>With us you can</h1>
-            </div>
-        </div>
-    </div>
+	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+		<header class="mdl-layout__header">
+			<div class="mdl-layout__header-row">
+				<!-- Title -->
+				<span class="mdl-layout-title">Code-Review</span>
+				<!-- Add spacer, to align navigation to the right -->
+				<div class="mdl-layout-spacer"></div>
+				<nav class="mdl-navigation">
+					<a class="mdl-navigation__link" href="">Log In</a>
+					<a class="mdl-navigation__link" href="">Sign In</a>
+				</nav>
+			</div>
+		</header>
+		<div class="info">
+			<div class="info__text">
+				<h1>With us you can</h1>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -33,13 +33,13 @@ $darkGrey: #202124;
 $blue: rgb(68,138,255);
 
 .info{
-    background-color: $darkGrey;
-    color: $white;
-    height: 700px;
+	background-color: $darkGrey;
+	color: $white;
+	height: 700px;
 }
 .info__text{
-    padding-top: 100px;
-    text-align: center;
+	padding-top: 100px;
+	text-align: center;
 }
 
 </style>
